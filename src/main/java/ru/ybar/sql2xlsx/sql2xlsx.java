6d753c8c9fed2a6b4235f04f75ac3f6929cd79c8
Slawka <slawka@eth0.org.ru>
2020-01-22 21:07:18 +0300

    Patch

--- src/main/java/ru/ybar/sql2xlsx/sql2xlsx.java
+++ src/main/java/ru/ybar/sql2xlsx/sql2xlsx.java
@@ -38,15 +38,6 @@ public class sql2xlsx {
         // Create a Row
         int rowNum = 0;
         Row headerRow = sheet.createRow(rowNum);
-        CellStyle backgroundRed = workbook.createCellStyle();
-        backgroundRed.setFillBackgroundColor(IndexedColors.RED.getIndex());
-        backgroundRed.setFillForegroundColor(IndexedColors.RED.getIndex());
-        backgroundRed.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-        CellStyle backgroundWhite = workbook.createCellStyle();
-        backgroundWhite.setFillBackgroundColor(IndexedColors.WHITE.getIndex());
-        backgroundWhite.setFillForegroundColor(IndexedColors.WHITE.getIndex());
-        backgroundWhite.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
         ResultSetMetaData metaData = result.getMetaData();
         int colCount = metaData.getColumnCount();
@@ -58,27 +49,11 @@ public class sql2xlsx {
         Cell cell;
         while (result.next()) {
             rowNum++;
-            /*            if (rowNum % 2 == 0) {
-                backgroundStyle.setFillBackgroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
-            } else {
-                backgroundStyle.setFillBackgroundColor(IndexedColors.WHITE.getIndex());
-            }
-             */
             Row row = sheet.createRow(rowNum);
             for (int i = 0; i < colCount; i++) {
-
                 cell = row.createCell(i);
                 cell.setCellValue(result.getString(i + 1));
-                if (sheetName.equals("Out")) {
-                    if (!result.getString(1).equals(result.getString(i + 1)) && i > 0) {
-
-                        cell.setCellStyle(backgroundRed);
-                    } else {
-
-                        cell.setCellStyle(backgroundWhite);
-                    }
-                }
-            }
+           }
         }
         for (int i = 0; i < colCount; i++) {
             sheet.autoSizeColumn(i);
